0c867f54e557f0e78b8b35b73157083891d32eea
Martin-J-Larre <martinjlarre@gmail.com>
2023-01-07 01:02:45 -0500

    #8 local storage added

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ const darkMode = () => {
   toggleIcon.children[0].textContent = 'Dark Mode';
   toggleIcon.children[1].classList.remove('fa-sun');
   toggleIcon.children[1].classList.add('fa-moon');
+  img1.src = './images/undraw_designer_life_re_dark.svg';
+  img2.src = './images/undraw_product_explainer_dark.svg';
+  img3.src = './images/undraw_virtual_reality_re_dark.svg';
 }
 const lightMode = () => { 
   nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
@@ -19,16 +22,30 @@ const lightMode = () => {
   toggleIcon.children[0].textContent = 'Light Mode';
   toggleIcon.children[1].classList.remove('fa-moon');
   toggleIcon.children[1].classList.add('fa-sun');
+  img1.src = './images/undraw_designer_life_re_light.svg';
+  img2.src = './images/undraw_product_explainer_light.svg';
+  img3.src = './images/undraw_virtual_reality_re_light.svg';
 }
 
 const switchTheme = (e) => { 
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
     darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
     lightMode();
   }
 }
 
 toggleSwitch.addEventListener('change', switchTheme);
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+} 
