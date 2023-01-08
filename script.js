c45495c28a951866f84e0553e7a8de6088af3bfb
Martin-J-Larre <martinjlarre@gmail.com>
2023-01-08 00:37:04 -0500

    #9 text contain added

--- script.js
+++ script.js
@@ -4,38 +4,32 @@ const textBox = document.getElementById('text-box');
 const toggleIcon = document.getElementById('toggle-icon');
 const img1 = document.getElementById('img1');
 const img2 = document.getElementById('img2');
-const img3 = document.getElementById('img3');
+const img3 = document.getElementById('img3');  
 
-const darkMode = () => { 
-  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'
-  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)'
-  toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classList.remove('fa-sun');
-  toggleIcon.children[1].classList.add('fa-moon');
-  img1.src = './images/undraw_designer_life_re_dark.svg';
-  img2.src = './images/undraw_product_explainer_dark.svg';
-  img3.src = './images/undraw_virtual_reality_re_dark.svg';
+const imageMode = (theme) => { 
+  img1.src = `./images/undraw_designer_life_re_${theme}.svg`;
+  img2.src = `./images/undraw_product_explainer_${theme}.svg`;
+  img3.src = `./images/undraw_virtual_reality_re_${theme}.svg`;
 }
-const lightMode = () => { 
-  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classList.remove('fa-moon');
-  toggleIcon.children[1].classList.add('fa-sun');
-  img1.src = './images/undraw_designer_life_re_light.svg';
-  img2.src = './images/undraw_product_explainer_light.svg';
-  img3.src = './images/undraw_virtual_reality_re_light.svg';
+console.log(img1);
+
+const toggleTheme = (dark) => { 
+  nav.style.backgroundColor = dark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = dark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)'; 
+  toggleIcon.children[0].textContent = dark ? 'Dark Mode' : 'Light Mode';
+  dark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  dark ? imageMode('dark') : imageMode('light');
 }
 
 const switchTheme = (e) => { 
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
-    darkMode();
+    toggleTheme(true)
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
-    lightMode();
+    toggleTheme(false)
   }
 }
 
@@ -46,6 +40,6 @@ if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
   if (currentTheme === 'dark') {
     toggleSwitch.checked = true;
-    darkMode();
+    toggleTheme(true);
   }
 } 
